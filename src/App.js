821d0df1fc140173cd3fb9c2e0bd96289ac1df47
JonathanMary <jon85ma@gmail.com>
2021-01-22 19:05:35 +0100

    MVP ..?

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from 'styled-components';
+//axios for API call
+import axios from '../node_modules/axios';
+//Characters for card design
+import Character from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [list, setList] = useState([]);
 
+  //const [isBusy, setIsBusy] = useState(true);
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+    .then(call => {
+      //call.data.results returns a list of 20 Pokemons individual url.
+      //will push the urls into getList and return it
+      //console.log(call.data.results);
+         let getList = [];
+         (call.data.results).forEach(pok => {
+            getList.push(pok)
+         });
+         setList(getList);
+         console.log(getList)
+         //return getList;
+         })
+         /*.then(urlList => {
+           let pokedex = [];
+           //console.log(urlList);
+           urlList.forEach(url => {
+             axios.get(`${url}`)
+                  .then(result => pokedex.push(result.data))
+                  .catch(err => console.log(err))
+           })
+           console.log("pokedex", pokedex);
+
+           setList(pokedex);
+         })*/
+         .catch(err => console.log(err))
+  }, []);
+
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Card>
+      {
+        list.map(pokemon => {
+          console.log("inside map", pokemon.name);
+          return <Character key={pokemon.name} name={pokemon.name} />;
+        })
+      }
+      </Card>
     </div>
   );
 }
 
 export default App;
+
+
+const Card = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  .card{
+    background-color: white;
+    padding: 2vw;
+    width: 16vw;
+    margin: 2vw;
+  }
+`
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
+import React from 'react';
 // Write your Character component here
+
+
+export default function Character(props){
+    //console.log("it runs");
+    const { name } = props;
+
+    return (
+        <div className="card">
+            <div className="card-top">
+                <h2>{name}</h2>
+                <h3>props.Types</h3>
+            </div>
+            <div className="picture">
+
+            </div>
+            <div className="skills">
+                <div className="skill-1">props.First Skill</div>
+                <div className="skill-2">props.Second Skill</div>
+            </div>
+        </div>
+    );
+}
